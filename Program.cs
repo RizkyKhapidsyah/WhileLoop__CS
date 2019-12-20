f7257b38522dd48ba87c9ca3f9449bd00e783bc9
Rizky Khapidsyah <rizkyhafitsyah@gmail.com>
2019-12-20 12:19:12 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace WhileLoop__CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y, A;
+            string Z;
+
+            X = 1;
+
+            Console.Write("Jumlah Perulangan Nilai Yang Ingin Ditampilkan   : ");
+            Y = Convert.ToInt32(Z = Console.ReadLine());
+            Console.Write("Jumlah Kelipatan                                 : ");
+            A = Convert.ToInt32(Z = Console.ReadLine());
+
+            Console.WriteLine();
+
+            while (X <= Y)
+            {
+                Console.WriteLine("Nilainya adalah = " + X);
+                X += A;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
